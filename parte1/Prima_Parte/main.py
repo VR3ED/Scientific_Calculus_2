6778d525ca24dda7170b3024cf8819129e693ef6
Francesco Cavallini <73038862+VR3ED@users.noreply.github.com>
2025-01-25 18:10:21 +0100

    conclusioni relazione 2 parte 1

--- parte1/Prima_Parte/main.py
+++ parte1/Prima_Parte/main.py
@@ -5,5 +5,5 @@ print("Esecuzione TEST per DCT e DCT2")
 test.run_test()
 
 # Testiamo con valori di N crescenti le performance di DCT home made e la DCT di Scipy
-times_scipy_dct, times_my_dct, matrix_dimensions = test.test_N()
-generate_graphs.plot_dct_times(times_scipy_dct, times_my_dct, matrix_dimensions)
+#times_scipy_dct, times_my_dct, matrix_dimensions = test.test_N()
+#generate_graphs.plot_dct_times(times_scipy_dct, times_my_dct, matrix_dimensions)
